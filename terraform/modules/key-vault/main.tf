fdea2331ca345e06c7ceddfd651f67c69bb3f853
shaharst-tr <shaharst@etoro.com>
2025-04-16 23:15:13 +0300

    fix sp obid

--- terraform/modules/key-vault/main.tf
+++ terraform/modules/key-vault/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_key_vault" "kv" {
 resource "azurerm_key_vault_access_policy" "terraform" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = "aa3d6aa5-e96a-4eb8-be74-cadb2e504e36"
+  object_id    = "1bc1919a-ec41-4e98-bd48-a455b1f65a4d"
 
   secret_permissions = [
     "Get",
